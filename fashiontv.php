5f289e7ba0ae7646101c82c82551960b83e891c7
Hemant Karekar <hemant.v.karekar@gmail.com>
2021-10-12 10:47:40 +0530

    try commit

---
+++ fashiontv.php
@@ -0,0 +1,101 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <?php include "includes/_head.php"; ?>
+    <title>Fashion TV | FTV School Of Grooming</title>
+    <link href="/assets/css/style.css" rel="stylesheet">
+</head>
+
+<body>
+    <?php include "includes/_nav.php"; ?>
+    <main class="fashiontv">
+        <section class="intro">
+            <div class="wrap">
+                <video class="lg">
+                    <source src="" type="">
+                </video>
+                <video class="md">
+                    <source src="" type="">
+                </video>
+                <img src="/assets/media/images/new-background-models.jpg" alt="">
+                <div class="video-title">
+                    <h1>School of Grooming</h1>
+                </div>
+            </div>
+            <div class="yellow patch fixed"></div>
+            <div class="yellow patch overlay"></div>
+        </section>
+        <section class="title">
+            <div class="content-wrap">
+                <div class="media-content">
+                    <div class="wrap">
+                        <img src="/assets/media/images/new-background-models.jpg" alt="">
+                    </div>
+                </div>
+                <div class="text-content">
+                    <div class="wrap">
+                        <div class="heading">
+                            <h1>Fashion TV</h1>
+                        </div>
+                        <div class="sub">
+                            <h2>School Of Grooming</h2>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="gray patch-overlay"></div>
+        </section>
+        <section class="introduction">
+            <div class="content-wrap">
+                <div class="media-content">
+                    <div class="wrap">
+                        <video class="lg">
+                            <source src="" type="">
+                        </video>
+                        <video class="md">
+                            <source src="" type="">
+                        </video>
+                        <img src="/assets/media/images/new-background-models.jpg" alt="">
+                    </div>
+                </div>
+                <div class="text-content">
+                    <div class="wrap">
+                        <div class="heading">
+                            <h1>Introduction</h1>
+                        </div>
+                        <div class="desc">
+                            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Deserunt voluptatibus minima vero ab architecto enim omnis vitae ut nam expedita.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="grey patch"></div>
+            </div>
+        </section>
+        <section class="key-facts">
+            <div class="content-wrap">
+                <div class="title">
+                    <h2>Key Facts</h1>
+                    <h1>sog</h1>
+                </div>
+                <div class="tab-pane">
+                    <div class="wrap">
+                        <div class="tab-content"></div>
+                        <div class="tab-nav">
+                            <ul>
+                                <li class="active">Key Facts</li>
+                                <li>Milestones</li>
+                                <li>New Media</li>
+                                <li>Future</li>
+                                <li>Conclusion</li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </main>
+    <?php include "includes/_footer.php"; ?>
+</body>
+
+</html>
---
+++ includes/_footer.php
@@ -0,0 +1,35 @@
+<footer>
+    <div class="foo-content">
+        <div class="main-wrap">
+            <div class="subs">
+                <div class="title">
+                    <h1><span>Subscribe</span> to our newsletter</h1>
+                </div>
+                <div class="subs-form">
+                    <form action="">
+                        <input type="email" name="email">
+                        <button type="submit"><img src="/assets/media/images/arrow.png" alt=""></button>
+                    </form>
+                </div>
+            </div>
+            <div class="links">
+                <span><a href="#">Careers</a></span>
+                <span><a href="#">Privacy Policy</a></span>
+                <span><a href="#">Terms & Conditions</a></span>
+            </div>
+        </div>
+        <div class="socials">
+            <div class="links-wrap">
+                <div class="link">
+                    <a href=""><img src="/assets/media/images/fb-f-white.png" alt=""></a>
+                </div>
+                <div class="link">
+                    <a href=""><img src="/assets/media/images/instagram-white.png" alt=""></a>
+                </div>
+            </div>
+        </div>
+        <div class="f-logo">
+            <img src="/assets/media/images/f-logo-black.png" alt="">
+        </div>
+    </div>
+</footer>
